f2b118a4ba7d89f5caf2066aa2eb09cea10977c6
Johannes Klever <johannes.klever2013@gmail.com>
2021-02-28 13:29:31 +0100

    Neues Profil erstellen + PW überprüfung, noch nicht abgeschlossen

--- Registrierung.vb
+++ Registrierung.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Windows.Forms
 
+
 Public Class Registrierung
 
 
@@ -24,15 +25,47 @@ Public Class Registrierung
                 MsgBox("Keine Daten gefunden!")
             End If
 
-            'rs.Close()
-            'conn.Close()
         Catch ex As Exception
             MsgBox(ex.Message)
         End Try
     End Sub
     Private Sub Button_Registrieren_Click(sender As Object, e As EventArgs) Handles Button_Registrieren.Click
-        rs.Fields("Benutzername").Value = TextBox_NeuerBenutzername.Text
+
+        If TextBox_NeuesPasswort.Text = TextBox_PasswortBestätigen.Text Then
+            rs.Fields("Benutzername").Value = TextBox_NeuerBenutzername.Text
+            rs.Fields("Passwort").Value = TextBox_NeuesPasswort
+        End If
+
 
     End Sub
+    'Public Function isValidPassword(ByVal Pwd As String) As Boolean
+    '    <System.Runtime.CompilerServices.Extension>
+    '    Return Pwd.Length > 7 AndAlso Pwd Like "[A-Z]*#*"
+    'End Function
+    Function ValidatePassword(ByVal pwd As String,
+    Optional ByVal minLength As Integer = 8,
+    Optional ByVal numUpper As Integer = 2,
+    Optional ByVal numLower As Integer = 2,
+    Optional ByVal numNumbers As Integer = 2,
+    Optional ByVal numSpecial As Integer = 2) As Boolean
+
+        ' Replace [A-Z] with \p{Lu}, to allow for Unicode uppercase letters.
+        Dim upper As New System.Text.RegularExpressions.Regex("[A-Z]")
+        Dim lower As New System.Text.RegularExpressions.Regex("[a-z]")
+        Dim number As New System.Text.RegularExpressions.Regex("[0-9]")
+        ' Special is "none of the above".
+        Dim special As New System.Text.RegularExpressions.Regex("[^a-zA-Z0-9]")
+
+        ' Check the length.
+        If Len(pwd) < minLength Then Return False
+        ' Check for minimum number of occurrences.
+        If upper.Matches(pwd).Count < numUpper Then Return False
+        If lower.Matches(pwd).Count < numLower Then Return False
+        If number.Matches(pwd).Count < numNumbers Then Return False
+        If special.Matches(pwd).Count < numSpecial Then Return False
+
+        ' Passed all checks.
+        Return True
+    End Function
 
 End Class
